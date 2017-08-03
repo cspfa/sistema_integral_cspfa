3a1a8f8b15bc364e7405aa2c626c6aa31781d222
CSPFA <cspfaweb@gmail.com>
2017-08-03 09:40:31 -0300

    AGREGO CONEXION REMOTA SELECCIONADA POR ROLE A CLASE CONNSTRING

--- conString.cs
+++ conString.cs
@@ -24,5 +24,32 @@ namespace SOCIOS
             connectionString = cs.ToString();
             return connectionString;
         }
+
+        public string getRemota(string ROL)
+        {
+            string connectionString = string.Empty;
+            Datos_ini ini2 = new Datos_ini();
+            FbConnectionStringBuilder cs = new FbConnectionStringBuilder();
+            
+            if (ROL == "CPOCABA")
+            {
+                cs.DataSource = ini2.Servidor_Delfo;
+                cs.Database = ini2.Ubicacion_Delfo;
+            }
+
+            if (ROL == "CPOPOLVORINES")
+            {
+                cs.DataSource = ini2.Servidor_Belgrano;
+                cs.Database = ini2.Ubicacion_Belgrano;
+            }
+
+            cs.Port = int.Parse(ini2.Puerto);
+            cs.UserID = VGlobales.vp_username;
+            cs.Password = VGlobales.vp_password;
+            cs.Role = VGlobales.vp_role;
+            cs.Dialect = 3;
+            connectionString = cs.ToString();
+            return connectionString;
+        }
     }
 }
