cb2026ac7b12aa989c31f7b89d763e7a1fe106dd
CSPFA <cspfaweb@gmail.com>
2017-09-12 14:56:20 -0300

    INGRESOS (NO SE PORQUE ME PIDE DE AGREGARLOS)

---
+++ ingresos.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SOCIOS
+{
+    partial class ingresos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ingresos));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dpDesde = new System.Windows.Forms.DateTimePicker();
+            this.dpHasta = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbRoles = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.dgIngresos = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbAnulado = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbDest = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgIngresos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FECHA DESDE";
+            // 
+            // dpDesde
+            // 
+            this.dpDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dpDesde.Location = new System.Drawing.Point(107, 13);
+            this.dpDesde.Name = "dpDesde";
+            this.dpDesde.Size = new System.Drawing.Size(105, 20);
+            this.dpDesde.TabIndex = 1;
+            // 
+            // dpHasta
+            // 
+            this.dpHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dpHasta.Location = new System.Drawing.Point(323, 13);
+            this.dpHasta.Name = "dpHasta";
+            this.dpHasta.Size = new System.Drawing.Size(105, 20);
+            this.dpHasta.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FECHA HASTA";
+            // 
+            // cbRoles
+            // 
+            this.cbRoles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRoles.FormattingEnabled = true;
+            this.cbRoles.Location = new System.Drawing.Point(639, 13);
+            this.cbRoles.Name = "cbRoles";
+            this.cbRoles.Size = new System.Drawing.Size(349, 21);
+            this.cbRoles.TabIndex = 4;
+            this.cbRoles.SelectedIndexChanged += new System.EventHandler(this.cbRoles_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(588, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ROLE";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(994, 12);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(156, 23);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "ACEPTAR";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // dgIngresos
+            // 
+            this.dgIngresos.AllowUserToAddRows = false;
+            this.dgIngresos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.dgIngresos.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgIngresos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgIngresos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgIngresos.Location = new System.Drawing.Point(13, 71);
+            this.dgIngresos.Name = "dgIngresos";
+            this.dgIngresos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgIngresos.Size = new System.Drawing.Size(1168, 531);
+            this.dgIngresos.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1165, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.TabIndex = 46;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(994, 39);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(156, 23);
+            this.btnExcel.TabIndex = 47;
+            this.btnExcel.Text = "EXPORTAR A EXCEL";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(443, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "ANULADO";
+            // 
+            // cbAnulado
+            // 
+            this.cbAnulado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAnulado.FormattingEnabled = true;
+            this.cbAnulado.Location = new System.Drawing.Point(517, 13);
+            this.cbAnulado.Name = "cbAnulado";
+            this.cbAnulado.Size = new System.Drawing.Size(56, 21);
+            this.cbAnulado.TabIndex = 48;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(589, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 51;
+            this.label5.Text = "DEST";
+            // 
+            // cbDest
+            // 
+            this.cbDest.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDest.FormattingEnabled = true;
+            this.cbDest.Location = new System.Drawing.Point(639, 40);
+            this.cbDest.Name = "cbDest";
+            this.cbDest.Size = new System.Drawing.Size(349, 21);
+            this.cbDest.TabIndex = 50;
+            // 
+            // ingresos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1194, 615);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbDest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbAnulado);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgIngresos);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbRoles);
+            this.Controls.Add(this.dpHasta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dpDesde);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ingresos";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listado de Ingresos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgIngresos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dpDesde;
+        private System.Windows.Forms.DateTimePicker dpHasta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbRoles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.DataGridView dgIngresos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbAnulado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbDest;
+    }
+}
---
+++ ingresos.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using FirebirdSql.Data.FirebirdClient;
+using System.IO;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SOCIOS
+{
+    public partial class ingresos : Form
+    {
+        bo dlog = new bo(); 
+
+        public ingresos()
+        {
+            InitializeComponent();
+            comboRoles();
+            comboDestinos(cbRoles.SelectedValue.ToString());
+            comboAnulado();
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        public void excel(DataSet ds, string path)
+        {
+            string data = null;
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add();
+            xlWorkSheet = xlWorkBook.Worksheets[1];
+            
+            xlWorkSheet.Range["A1:Z1"].Font.Bold = true;
+
+            xlWorkSheet.Cells[1, 1] = "#";
+            xlWorkSheet.Cells[1, 2] = "TIPO";
+            xlWorkSheet.Cells[1, 3] = "FECHA RECIBO";
+            xlWorkSheet.Cells[1, 4] = "FECHA CAJA";
+            xlWorkSheet.Cells[1, 5] = "FECHA ALTA";
+            xlWorkSheet.Cells[1, 6] = "FECHA ANULADO";
+            xlWorkSheet.Cells[1, 7] = "CUENTA DEBE";
+            xlWorkSheet.Cells[1, 8] = "CUENTA HABER";
+            xlWorkSheet.Cells[1, 9] = "VALOR";
+            xlWorkSheet.Cells[1, 10] = "PAGO";
+            xlWorkSheet.Cells[1, 11] = "OBSERVACIONES";
+            xlWorkSheet.Cells[1, 12] = "SOCIO TITULAR";
+            xlWorkSheet.Cells[1, 13] = "TIPO TITULAR";
+            xlWorkSheet.Cells[1, 14] = "SOCIO";
+            xlWorkSheet.Cells[1, 15] = "TIPO";
+            xlWorkSheet.Cells[1, 16] = "BARRA";
+            xlWorkSheet.Cells[1, 17] = "SERVICIO";
+            xlWorkSheet.Cells[1, 18] = "PROFESIONAL";
+            xlWorkSheet.Cells[1, 19] = "ROL";
+            xlWorkSheet.Cells[1, 20] = "DNI";
+            
+            for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                for (int j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                {
+                    data = ds.Tables[0].Rows[i].ItemArray[j].ToString().Trim();
+                    xlWorkSheet.Cells[i + 2, j + 1] = data;
+                    xlWorkSheet.Columns[j + 1].AutoFit();
+                    xlWorkSheet.Columns[3].EntireColumn.NumberFormat = "DD/MM/AAAA";
+                }
+            }
+
+            xlWorkBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        public void comboDestinos(string ROL)
+        {
+            string query;
+            query = "SELECT ID, DETALLE FROM SECTACT WHERE ROL = '" + ROL + "' AND ESTADO = 1 ORDER BY DETALLE ASC;";
+            cbDest.DataSource = null;
+            cbDest.Items.Clear();
+            cbDest.SelectedItem = 0;
+            cbDest.DataSource = dlog.BO_EjecutoDataTable(query);
+            cbDest.DisplayMember = "DETALLE";
+            cbDest.ValueMember = "ID";
+        }
+
+        public void comboRoles()
+        {
+            string query;
+
+            if (VGlobales.vp_role == "SISTEMAS" || VGlobales.vp_role == "CAJA" || VGlobales.vp_role == "CONTADURIA")
+            {
+                query = "SELECT DISTINCT ROL FROM SECTACT ORDER BY ROL;";
+            }
+            else if (VGlobales.vp_role == "SERVICIOS MEDICOS")
+            {
+                query = "SELECT DISTINCT ROL FROM SECTACT WHERE ROL = 'SERVICIOS MEDICOS' OR ROL = 'RECETAS' ORDER BY ROL;";
+            }
+            else
+            {
+                query = "SELECT DISTINCT ROL FROM SECTACT WHERE ROL = '" + VGlobales.vp_role + "' ORDER BY ROL;";
+            }
+
+            cbRoles.DataSource = null;
+            cbRoles.Items.Clear();
+            cbRoles.SelectedItem = 0;
+            cbRoles.DataSource = dlog.BO_EjecutoDataTable(query);
+            cbRoles.DisplayMember = "ROL";
+            cbRoles.ValueMember = "ROL";
+        }
+
+        public void comboAnulado()
+        {
+            cbAnulado.Items.Clear();
+            cbAnulado.Items.Add("NO");
+            cbAnulado.Items.Add("SI");
+            cbAnulado.SelectedIndex = 0;
+        }
+
+        static void OpenMicrosoftExcel(string f)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "EXCEL.EXE";
+            startInfo.Arguments = f;
+            Process.Start(startInfo);
+        }
+
+        private void mostrarRecibos(FbDataReader reader3)
+        {
+            DataSet ds1 = new DataSet();
+            DataTable dt1 = new DataTable("RESULTADOS");
+            ds1.Tables.Add(dt1);
+            dt1.Columns.Add("#", typeof(int));
+            dt1.Columns.Add("TIPO", typeof(string));
+            dt1.Columns.Add("FECHA RECIBO", typeof(string));
+            dt1.Columns.Add("FECHA CAJA", typeof(string));
+            dt1.Columns.Add("FECHA ALTA", typeof(string));
+            dt1.Columns.Add("FECHA ANULADO", typeof(string));
+            dt1.Columns.Add("CUENTA DEBE", typeof(int));
+            dt1.Columns.Add("CUENTA HABER", typeof(int));
+            dt1.Columns.Add("VALOR", typeof(decimal));
+            dt1.Columns.Add("PAGO", typeof(string));
+            dt1.Columns.Add("OBSERVACIONES", typeof(string));
+            dt1.Columns.Add("NOMBRE TITULAR", typeof(string));
+            dt1.Columns.Add("TIPO TITULAR", typeof(string));
+            dt1.Columns.Add("NOMBRE SOCIO", typeof(string));
+            dt1.Columns.Add("TIPO SOCIO", typeof(string));
+            dt1.Columns.Add("/", typeof(string));
+            dt1.Columns.Add("DETALLE", typeof(string));
+            dt1.Columns.Add("PROFESIONAL", typeof(string));
+            dt1.Columns.Add("ROLE", typeof(string));
+            dt1.Columns.Add("DNI", typeof(string));
+
+            string VALOR = string.Empty;
+            decimal IMPORTE;
+
+            while (reader3.Read())
+            {
+                string ID = reader3.GetString(reader3.GetOrdinal("NRO_COMP"));
+                string BR = reader3.GetString(reader3.GetOrdinal("BR")).Trim();
+                string FECHA_RECIBO = reader3.GetString(reader3.GetOrdinal("FECHA_RECIBO")).Substring(0, 10);
+                string FECHA_CAJA = reader3.GetString(reader3.GetOrdinal("FECHA_CAJA"));
+                string FECHA_ALTA = reader3.GetString(reader3.GetOrdinal("FECHA_ALTA"));
+                string FECHA_ANULADO = reader3.GetString(reader3.GetOrdinal("FECHA_ANULADO"));
+                string CUENTA_DEBE = reader3.GetString(reader3.GetOrdinal("CUENTA_DEBE")).Trim();
+                string CUENTA_HABER = reader3.GetString(reader3.GetOrdinal("CUENTA_HABER")).Trim();
+                IMPORTE = reader3.GetDecimal(reader3.GetOrdinal("VALOR"));
+                VALOR = string.Format("{0:n}", IMPORTE).Trim();
+                string PAGO = reader3.GetString(reader3.GetOrdinal("PAGO")).Trim();
+                string OBSERVACIONES = reader3.GetString(reader3.GetOrdinal("OBSERVACIONES")).Trim();
+                string NOMBRE_SOCIO_TITULAR = reader3.GetString(reader3.GetOrdinal("NOMBRE_SOCIO_TITULAR")).Trim();
+                string TIPO_SOCIO_TITULAR = reader3.GetString(reader3.GetOrdinal("TIPO_SOCIO_TITULAR")).Trim();
+                string NOMBRE_SOCIO = reader3.GetString(reader3.GetOrdinal("NOMBRE_SOCIO")).Trim();
+                string TIPO_SOCIO = reader3.GetString(reader3.GetOrdinal("TIPO_SOCIO")).Trim();
+                string BARRA = reader3.GetString(reader3.GetOrdinal("BARRA"));
+                string DETAIL = reader3.GetString(reader3.GetOrdinal("DETALLE")).Trim();
+                string PROFESIONAL = reader3.GetString(reader3.GetOrdinal("PROFESIONAL")).Trim();
+                string ROLL = reader3.GetString(reader3.GetOrdinal("ROLL")).Trim();
+                string DNI = reader3.GetString(reader3.GetOrdinal("DNI"));
+
+                dt1.Rows.Add(ID, BR, FECHA_RECIBO, FECHA_CAJA, FECHA_ALTA, FECHA_ANULADO, CUENTA_DEBE, CUENTA_HABER, VALOR, PAGO, OBSERVACIONES, NOMBRE_SOCIO_TITULAR, TIPO_SOCIO_TITULAR,
+                    NOMBRE_SOCIO, TIPO_SOCIO, BARRA, DETAIL, PROFESIONAL, ROLL, DNI);
+            }
+
+            dgIngresos.DataSource = dt1;
+            reader3.Close();
+        }
+
+        private void buscarRecibos(string DESDE, string HASTA, string ROL, string COMANDO, string ANULADO, string DETALLE)
+        {
+            try
+            {
+                string connectionString;
+                Datos_ini ini3 = new Datos_ini();
+                string query = "SELECT * FROM INGRESOS ('" + DESDE + "', '" + HASTA + "', '" + ROL + "', '" + ANULADO + "', '" + DETALLE + "');";
+                FbConnectionStringBuilder cs = new FbConnectionStringBuilder();
+                cs.DataSource = ini3.Servidor;  cs.Port = int.Parse(ini3.Puerto);
+                cs.Database = ini3.Ubicacion;
+                cs.UserID = VGlobales.vp_username;
+                cs.Password = VGlobales.vp_password;
+                cs.Role = VGlobales.vp_role;
+                cs.Dialect = 3;
+                connectionString = cs.ToString();
+
+                using (FbConnection connection = new FbConnection(connectionString))
+                {
+                    connection.Open();
+                    FbTransaction transaction = connection.BeginTransaction();
+                    FbCommand cmd = new FbCommand(query, connection, transaction);
+                    FbDataReader reader3 = cmd.ExecuteReader();
+
+                        if (COMANDO == "VER")
+                        {
+                            mostrarRecibos(reader3);
+                        }
+
+                        if (COMANDO == "EXCEL")
+                        {
+                            bo dlog = new bo();
+                            DataSet ds = dlog.BO_EjecutoDataSet(query);
+                            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                            saveFileDialog1.Filter = "Archivo XLS|*.xls";
+                            saveFileDialog1.Title = "Guardar Listado";
+
+                            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                            {
+                                Cursor = Cursors.WaitCursor;
+                                excel(ds, saveFileDialog1.FileName);
+                                Cursor = Cursors.Default;
+                                DialogResult result = MessageBox.Show("LISTADO GENERADO CORRECTAMENTE \n\n ¿ABRIR EL ARCHIVO?", "LISTO!", MessageBoxButtons.YesNo);
+
+                                if (result == DialogResult.Yes)
+                                {
+                                    OpenMicrosoftExcel(saveFileDialog1.FileName);
+                                }
+                            }
+                        }
+
+                    transaction.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            string DIAD = dpDesde.Text.Substring(0, 2);
+            string MESD = dpDesde.Text.Substring(3, 2);
+            string ANOD = dpDesde.Text.Substring(6, 4);
+            string FECHA_DESDE = MESD + "/" + DIAD + "/" + ANOD;
+
+            string DIAH = dpHasta.Text.Substring(0, 2);
+            string MESH = dpHasta.Text.Substring(3, 2);
+            string ANOH = dpHasta.Text.Substring(6, 4);
+            string FECHA_HASTA = MESH + "/" + DIAH + "/" + ANOH;
+
+            string ANULADO = cbAnulado.SelectedItem.ToString();
+            string DETALLE = cbDest.SelectedValue.ToString();
+            string ROL = cbRoles.SelectedValue.ToString().Trim();
+
+            buscarRecibos(FECHA_DESDE, FECHA_HASTA, ROL, "VER", ANULADO, DETALLE);
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            string DIAD = dpDesde.Text.Substring(0, 2);
+            string MESD = dpDesde.Text.Substring(3, 2);
+            string ANOD = dpDesde.Text.Substring(6, 4);
+            string FECHA_DESDE = MESD + "/" + DIAD + "/" + ANOD;
+
+            string DIAH = dpHasta.Text.Substring(0, 2);
+            string MESH = dpHasta.Text.Substring(3, 2);
+            string ANOH = dpHasta.Text.Substring(6, 4);
+            string FECHA_HASTA = MESH + "/" + DIAH + "/" + ANOH;
+
+            string ANULADO = cbAnulado.SelectedItem.ToString();
+            string DETALLE = cbDest.SelectedValue.ToString();
+            string ROL = cbRoles.SelectedValue.ToString().Trim();
+
+            buscarRecibos(FECHA_DESDE, FECHA_HASTA, ROL, "EXCEL", ANULADO, DETALLE);
+        }
+
+        private void cbRoles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboDestinos(cbRoles.SelectedValue.ToString());
+        }
+    }
+}
