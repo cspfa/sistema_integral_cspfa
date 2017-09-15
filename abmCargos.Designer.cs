ae9a1a74c3ac9ede41f1753780958138eaccda0d
CSPFA <cspfaweb@gmail.com>
2017-09-15 13:29:42 -0300

    ABM DE CARGOS Y ESCALAFONES PARA INGRESOS DE PERSONAS
    CREACION DE bo_IngresosPersonas

---
+++ abmCargos.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SOCIOS
+{
+    partial class abmCargos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCargo = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CARGO / PUESTO ";
+            // 
+            // tbCargo
+            // 
+            this.tbCargo.Location = new System.Drawing.Point(17, 36);
+            this.tbCargo.Name = "tbCargo";
+            this.tbCargo.Size = new System.Drawing.Size(213, 20);
+            this.tbCargo.TabIndex = 1;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(236, 34);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "NUEVO";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // abmCargos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 83);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.tbCargo);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "abmCargos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ABM CARGOS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCargo;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
---
+++ abmCargos.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using FirebirdSql.Data.FirebirdClient;
+using System.IO;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+
+namespace SOCIOS
+{
+    public partial class abmCargos : Form
+    {
+        BO.bo_IngresosPersonas IP = new BO.bo_IngresosPersonas();
+        private string TITULO_FORM { get; set; }
+        private string TITULO_LABEL { get; set; }
+        private string ERROR_TB { get; set; }
+        private string WHICH { get; set; }
+
+        public abmCargos(string CUAL)
+        {
+            InitializeComponent();
+            selector(CUAL);
+            this.Text = TITULO_FORM;
+            label1.Text = TITULO_LABEL;
+        }
+
+        private void selector(string CUAL)
+        { 
+            switch (CUAL)
+            {
+                case "CARGO":
+                    TITULO_FORM = "ALTA NUEVO CARGO / PUESTO";
+                    TITULO_LABEL = "NOMBRE DEL CARGO / PUESTO";
+                    ERROR_TB = "COMPLETAR EL CAMPO CARGO / PUESTO";
+                    WHICH = "CARGO";
+                    break;
+
+                case "ESCALAFON":
+                    TITULO_FORM = "ALTA NUEVO ESCALAFON";
+                    TITULO_LABEL = "NOMBRE DEL ESCALAFON";
+                    ERROR_TB = "COMPLETAR EL CAMPO ESCALAFON";
+                    WHICH = "ESCALAFON";
+                    break;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            if (tbCargo.Text == "")
+            {
+                MessageBox.Show(ERROR_TB, "ERROR");
+            }
+            else
+            {
+                try
+                {
+                    string CARGO = tbCargo.Text.Trim();
+                    IP.altaCargoEscalafon(CARGO, WHICH);
+                    MessageBox.Show("TODO SALIÓ PERFECTO :)", "LISTO");
+                    this.Close();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show("ALGO SALIÓ MAL :(\n"+error, "ERROR");
+                }
+            }
+        }
+    }
+}
