749922204f63399ec0801959b67c288322bbc1bd
CSPFA <cspfaweb@gmail.com>
2017-09-18 18:00:35 -0300

    EDAD

--- recibos/edad.cs
+++ recibos/edad.cs
@@ -11,18 +11,29 @@ namespace SOCIOS
 {
     class edad
     {
+        bo dlog = new bo();
+
         public int CALCULAR(string FECHA)
         {
-            string fecha_nacimiento = FECHA;
-            DateTime dt_fecha_nacimiento = Convert.ToDateTime(fecha_nacimiento);
-            int anio_nacimiento = dt_fecha_nacimiento.Year;
-            int anio_actual = DateTime.Now.Year;
-            int dif_anios = anio_actual - anio_nacimiento;
-            DateTime cumple_anios = dt_fecha_nacimiento.AddYears(dif_anios);
+            int dif_anios = 0;
 
-            if (DateTime.Now.CompareTo(cumple_anios) < 0)
+            try
             {
-                dif_anios--;
+                string fecha_nacimiento = FECHA;
+                DateTime dt_fecha_nacimiento = Convert.ToDateTime(fecha_nacimiento);
+                int anio_nacimiento = dt_fecha_nacimiento.Year;
+                int anio_actual = DateTime.Now.Year;
+                dif_anios = anio_actual - anio_nacimiento;
+                DateTime cumple_anios = dt_fecha_nacimiento.AddYears(dif_anios);
+
+                if (DateTime.Now.CompareTo(cumple_anios) < 0)
+                {
+                    dif_anios--;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
             }
 
             return dif_anios;
@@ -32,11 +43,18 @@ namespace SOCIOS
         {
             int edad = 0;
 
-            if (ALTVI != "")
+            try
             {
-                DateTime today = Convert.ToDateTime("07/03/" + DateTime.Today.Year);
-                DateTime bday = Convert.ToDateTime(ALTVI);
-                edad = today.Year - bday.Year;
+                if (ALTVI != "")
+                {
+                    DateTime today = Convert.ToDateTime("07/03/" + DateTime.Today.Year);
+                    DateTime bday = Convert.ToDateTime(ALTVI);
+                    edad = today.Year - bday.Year;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
             }
 
             return edad;
@@ -44,19 +62,26 @@ namespace SOCIOS
         
         public int VITALICIO_ORO(int ID_TITULAR)
         {
-            bo dlog = new bo();
-            string query = "SELECT F_ALTVI FROM TITULAR WHERE ID_TITULAR = " + ID_TITULAR;
-            DataRow[] foundRows;
-            foundRows = dlog.BO_EjecutoDataTable(query).Select();
             int dif_anios = 0;
 
-            if (foundRows.Length > 0)
+            try
             {
-                dif_anios = CALCULAR_VITALICIO(foundRows[0][0].ToString());
+                string query = "SELECT F_ALTVI FROM TITULAR WHERE ID_TITULAR = " + ID_TITULAR;
+                DataRow[] foundRows;
+                foundRows = dlog.BO_EjecutoDataTable(query).Select();
+
+                if (foundRows.Length > 0)
+                {
+                    dif_anios = CALCULAR_VITALICIO(foundRows[0][0].ToString());
+                }
+                else
+                {
+                    dif_anios = 9999;
+                }
             }
-            else
+            catch (Exception error)
             {
-                dif_anios = 9999;
+                MessageBox.Show(error.ToString());
             }
 
             return dif_anios;
@@ -64,20 +89,26 @@ namespace SOCIOS
         
         public int FAMILIAR(int ID_TITULAR, string BARRA)
         {
-            bo dlog = new bo();
-            string query = "SELECT F_NACFAM FROM FAMILIAR WHERE ID_TITULAR = " + ID_TITULAR + " AND BARRA = " + BARRA;
-            DataRow[] foundRows;
-            foundRows = dlog.BO_EjecutoDataTable(query).Select();
             int dif_anios = 0;
 
-            if (foundRows.Length > 0)
+            try
             {
-                if (foundRows[0][0].ToString().Length >0)
-                dif_anios = CALCULAR(foundRows[0][0].ToString());
+                string query = "SELECT F_NACFAM FROM FAMILIAR WHERE ID_TITULAR = " + ID_TITULAR + " AND BARRA = " + BARRA;
+                DataRow[] foundRows;
+                foundRows = dlog.BO_EjecutoDataTable(query).Select();
+
+                if (foundRows.Length > 0)
+                {
+                    dif_anios = CALCULAR(foundRows[0][0].ToString());
+                }
+                else
+                {
+                    dif_anios = 9999;
+                }
             }
-            else
+            catch (Exception error)
             {
-                dif_anios = 9999;
+                MessageBox.Show(error.ToString());
             }
 
             return dif_anios;
