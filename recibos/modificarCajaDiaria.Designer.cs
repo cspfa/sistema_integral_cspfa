a3bd630129494044b47dbba3a6ad2bdabcdc7996
CSPFA <cspfaweb@gmail.com>
2017-11-13 12:11:42 -0300

    FORMULARIO PARA MODIFICAR CAJAS YA CERRADAS POR ERRORES

--- recibos/modificarCajaDiaria.Designer.cs
+++ recibos/modificarCajaDiaria.Designer.cs
@@ -29,30 +29,32 @@
         private void InitializeComponent()
         {
             this.gbCaja = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.tbEfectivo = new System.Windows.Forms.TextBox();
-            this.tbOtros = new System.Windows.Forms.TextBox();
-            this.tbSubtotal = new System.Windows.Forms.TextBox();
-            this.tbEgresos = new System.Windows.Forms.TextBox();
-            this.tbSaldo = new System.Windows.Forms.TextBox();
-            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.lbDiferencia = new System.Windows.Forms.Label();
             this.btnModificar = new System.Windows.Forms.Button();
-            this.lbEfectivo = new System.Windows.Forms.Label();
-            this.lbOtros = new System.Windows.Forms.Label();
-            this.lbSubtotal = new System.Windows.Forms.Label();
-            this.lbEgresos = new System.Windows.Forms.Label();
-            this.lbSaldos = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.tbSaldo = new System.Windows.Forms.TextBox();
+            this.tbEgresos = new System.Windows.Forms.TextBox();
+            this.tbSubtotal = new System.Windows.Forms.TextBox();
+            this.tbOtros = new System.Windows.Forms.TextBox();
+            this.tbEfectivo = new System.Windows.Forms.TextBox();
             this.lbTotal = new System.Windows.Forms.Label();
+            this.lbSaldos = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbEgresos = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbSubtotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbOtros = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbEfectivo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.gbCaja.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbCaja
             // 
+            this.gbCaja.Controls.Add(this.lbDiferencia);
             this.gbCaja.Controls.Add(this.btnModificar);
             this.gbCaja.Controls.Add(this.tbTotal);
             this.gbCaja.Controls.Add(this.tbSaldo);
@@ -79,101 +81,14 @@
             this.gbCaja.TabStop = false;
             this.gbCaja.Text = "groupBox1";
             // 
-            // label1
+            // lbDiferencia
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 35);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "EFECTIVO";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(110, 35);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "OTROS";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(208, 35);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "SUBTOTAL";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(306, 35);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "EGRESOS";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(404, 35);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "SALDO";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(502, 35);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(42, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "TOTAL";
-            // 
-            // tbEfectivo
-            // 
-            this.tbEfectivo.Location = new System.Drawing.Point(12, 81);
-            this.tbEfectivo.Name = "tbEfectivo";
-            this.tbEfectivo.Size = new System.Drawing.Size(78, 20);
-            this.tbEfectivo.TabIndex = 6;
-            // 
-            // tbOtros
-            // 
-            this.tbOtros.Location = new System.Drawing.Point(110, 81);
-            this.tbOtros.Name = "tbOtros";
-            this.tbOtros.Size = new System.Drawing.Size(78, 20);
-            this.tbOtros.TabIndex = 7;
-            // 
-            // tbSubtotal
-            // 
-            this.tbSubtotal.Location = new System.Drawing.Point(208, 81);
-            this.tbSubtotal.Name = "tbSubtotal";
-            this.tbSubtotal.Size = new System.Drawing.Size(78, 20);
-            this.tbSubtotal.TabIndex = 8;
-            // 
-            // tbEgresos
-            // 
-            this.tbEgresos.Location = new System.Drawing.Point(306, 81);
-            this.tbEgresos.Name = "tbEgresos";
-            this.tbEgresos.Size = new System.Drawing.Size(78, 20);
-            this.tbEgresos.TabIndex = 9;
-            // 
-            // tbSaldo
-            // 
-            this.tbSaldo.Location = new System.Drawing.Point(404, 81);
-            this.tbSaldo.Name = "tbSaldo";
-            this.tbSaldo.Size = new System.Drawing.Size(78, 20);
-            this.tbSaldo.TabIndex = 10;
-            // 
-            // tbTotal
-            // 
-            this.tbTotal.Location = new System.Drawing.Point(502, 81);
-            this.tbTotal.Name = "tbTotal";
-            this.tbTotal.Size = new System.Drawing.Size(78, 20);
-            this.tbTotal.TabIndex = 11;
+            this.lbDiferencia.AutoSize = true;
+            this.lbDiferencia.Location = new System.Drawing.Point(593, 58);
+            this.lbDiferencia.Name = "lbDiferencia";
+            this.lbDiferencia.Size = new System.Drawing.Size(71, 13);
+            this.lbDiferencia.TabIndex = 13;
+            this.lbDiferencia.Text = "DIFERENCIA";
             // 
             // btnModificar
             // 
@@ -184,41 +99,58 @@
             this.btnModificar.Text = "MODIFICAR";
             this.btnModificar.UseVisualStyleBackColor = true;
             // 
-            // lbEfectivo
+            // tbTotal
             // 
-            this.lbEfectivo.AutoSize = true;
-            this.lbEfectivo.Location = new System.Drawing.Point(12, 58);
-            this.lbEfectivo.Name = "lbEfectivo";
-            this.lbEfectivo.Size = new System.Drawing.Size(59, 13);
-            this.lbEfectivo.TabIndex = 0;
-            this.lbEfectivo.Text = "EFECTIVO";
+            this.tbTotal.Location = new System.Drawing.Point(502, 81);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(78, 20);
+            this.tbTotal.TabIndex = 11;
             // 
-            // lbOtros
+            // tbSaldo
             // 
-            this.lbOtros.AutoSize = true;
-            this.lbOtros.Location = new System.Drawing.Point(110, 58);
-            this.lbOtros.Name = "lbOtros";
-            this.lbOtros.Size = new System.Drawing.Size(45, 13);
-            this.lbOtros.TabIndex = 1;
-            this.lbOtros.Text = "OTROS";
+            this.tbSaldo.Location = new System.Drawing.Point(404, 81);
+            this.tbSaldo.Name = "tbSaldo";
+            this.tbSaldo.Size = new System.Drawing.Size(78, 20);
+            this.tbSaldo.TabIndex = 10;
             // 
-            // lbSubtotal
+            // tbEgresos
             // 
-            this.lbSubtotal.AutoSize = true;
-            this.lbSubtotal.Location = new System.Drawing.Point(208, 58);
-            this.lbSubtotal.Name = "lbSubtotal";
-            this.lbSubtotal.Size = new System.Drawing.Size(64, 13);
-            this.lbSubtotal.TabIndex = 2;
-            this.lbSubtotal.Text = "SUBTOTAL";
+            this.tbEgresos.Location = new System.Drawing.Point(306, 81);
+            this.tbEgresos.Name = "tbEgresos";
+            this.tbEgresos.Size = new System.Drawing.Size(78, 20);
+            this.tbEgresos.TabIndex = 9;
             // 
-            // lbEgresos
+            // tbSubtotal
             // 
-            this.lbEgresos.AutoSize = true;
-            this.lbEgresos.Location = new System.Drawing.Point(306, 58);
-            this.lbEgresos.Name = "lbEgresos";
-            this.lbEgresos.Size = new System.Drawing.Size(59, 13);
-            this.lbEgresos.TabIndex = 3;
-            this.lbEgresos.Text = "EGRESOS";
+            this.tbSubtotal.Location = new System.Drawing.Point(208, 81);
+            this.tbSubtotal.Name = "tbSubtotal";
+            this.tbSubtotal.Size = new System.Drawing.Size(78, 20);
+            this.tbSubtotal.TabIndex = 8;
+            // 
+            // tbOtros
+            // 
+            this.tbOtros.Location = new System.Drawing.Point(110, 81);
+            this.tbOtros.Name = "tbOtros";
+            this.tbOtros.Size = new System.Drawing.Size(78, 20);
+            this.tbOtros.TabIndex = 7;
+            this.tbOtros.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbOtros_KeyUp);
+            // 
+            // tbEfectivo
+            // 
+            this.tbEfectivo.Location = new System.Drawing.Point(12, 81);
+            this.tbEfectivo.Name = "tbEfectivo";
+            this.tbEfectivo.Size = new System.Drawing.Size(78, 20);
+            this.tbEfectivo.TabIndex = 6;
+            this.tbEfectivo.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbEfectivo_KeyUp);
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Location = new System.Drawing.Point(502, 58);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(42, 13);
+            this.lbTotal.TabIndex = 5;
+            this.lbTotal.Text = "TOTAL";
             // 
             // lbSaldos
             // 
@@ -229,14 +161,95 @@
             this.lbSaldos.TabIndex = 4;
             this.lbSaldos.Text = "SALDO";
             // 
-            // lbTotal
+            // label6
             // 
-            this.lbTotal.AutoSize = true;
-            this.lbTotal.Location = new System.Drawing.Point(502, 58);
-            this.lbTotal.Name = "lbTotal";
-            this.lbTotal.Size = new System.Drawing.Size(42, 13);
-            this.lbTotal.TabIndex = 5;
-            this.lbTotal.Text = "TOTAL";
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(502, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "TOTAL";
+            // 
+            // lbEgresos
+            // 
+            this.lbEgresos.AutoSize = true;
+            this.lbEgresos.Location = new System.Drawing.Point(306, 58);
+            this.lbEgresos.Name = "lbEgresos";
+            this.lbEgresos.Size = new System.Drawing.Size(59, 13);
+            this.lbEgresos.TabIndex = 3;
+            this.lbEgresos.Text = "EGRESOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(404, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SALDO";
+            // 
+            // lbSubtotal
+            // 
+            this.lbSubtotal.AutoSize = true;
+            this.lbSubtotal.Location = new System.Drawing.Point(208, 58);
+            this.lbSubtotal.Name = "lbSubtotal";
+            this.lbSubtotal.Size = new System.Drawing.Size(64, 13);
+            this.lbSubtotal.TabIndex = 2;
+            this.lbSubtotal.Text = "SUBTOTAL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(306, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "EGRESOS";
+            // 
+            // lbOtros
+            // 
+            this.lbOtros.AutoSize = true;
+            this.lbOtros.Location = new System.Drawing.Point(110, 58);
+            this.lbOtros.Name = "lbOtros";
+            this.lbOtros.Size = new System.Drawing.Size(45, 13);
+            this.lbOtros.TabIndex = 1;
+            this.lbOtros.Text = "OTROS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SUBTOTAL";
+            // 
+            // lbEfectivo
+            // 
+            this.lbEfectivo.AutoSize = true;
+            this.lbEfectivo.Location = new System.Drawing.Point(12, 58);
+            this.lbEfectivo.Name = "lbEfectivo";
+            this.lbEfectivo.Size = new System.Drawing.Size(59, 13);
+            this.lbEfectivo.TabIndex = 0;
+            this.lbEfectivo.Text = "EFECTIVO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OTROS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EFECTIVO";
             // 
             // modificarCajaDiaria
             // 
@@ -276,5 +289,6 @@
         private System.Windows.Forms.Label lbSubtotal;
         private System.Windows.Forms.Label lbOtros;
         private System.Windows.Forms.Label lbEfectivo;
+        private System.Windows.Forms.Label lbDiferencia;
     }
 }
--- recibos/modificarCajaDiaria.cs
+++ recibos/modificarCajaDiaria.cs
@@ -17,24 +17,72 @@ namespace SOCIOS
             cargaInicial(ID, FECHA, EFECTIVO, OTROS, SUBTOTAL, EGRESOS, SALDO, TOTAL);
         }
 
+        private decimal sumar(decimal VALOR1, decimal VALOR2)
+        {
+            decimal SUMA = 0;
+            SUMA = VALOR1 + VALOR2;
+            return SUMA;
+        }
+
         private void cargaInicial(string ID, string FECHA, string EFECTIVO, string OTROS, string SUBTOTAL, string EGRESOS, string SALDO, string TOTAL)
         {
-            int INT_SUBTOTAL = int.Parse(EFECTIVO) + int.Parse(OTROS);
+            decimal INT_SUBTOTAL = decimal.Parse(EFECTIVO) + decimal.Parse(OTROS);
             gbCaja.Text = "MODIFICAR TOTALES DE CAJA DEL " + FECHA;
             
             tbEfectivo.Text = EFECTIVO;
             tbOtros.Text = OTROS;
-            tbSubtotal.Text = SUBTOTAL;
+            tbSubtotal.Text = string.Format("{0:n}", INT_SUBTOTAL);
             tbEgresos.Text = EGRESOS;
             tbSaldo.Text = SALDO;
             tbTotal.Text = TOTAL;
             
             lbEfectivo.Text = EFECTIVO;
             lbOtros.Text = OTROS;
-            lbSubtotal.Text = INT_SUBTOTAL.ToString();
+            lbSubtotal.Text = string.Format("{0:n}", INT_SUBTOTAL);
             lbEgresos.Text = EGRESOS;
             lbSaldos.Text = SALDO;
             lbTotal.Text = TOTAL;
+        }
+
+        private void calcular(TextBox SENDER, TextBox NOSENDER, Label ORIGINAL)
+        {
+            decimal VALOR1 = 0;
+            decimal VALOR2 = 0;
+            decimal SUBTOTAL = 0;
+            string RESULTADO = string.Empty;
+            decimal DIFERENCIA = 0;
+            decimal TOTAL = 0;
+            decimal VORIGINAL = 0;
+
+            try
+            {
+                SENDER.BackColor = Color.White;
+                VORIGINAL = decimal.Parse(SENDER.Text);
+                VALOR1 = decimal.Parse(SENDER.Text);
+                VALOR2 = decimal.Parse(NOSENDER.Text);
+                SUBTOTAL = sumar(VALOR1, VALOR2);
+                RESULTADO = string.Format("{0:n}", SUBTOTAL);
+                tbSubtotal.Text = RESULTADO;
+                tbSaldo.Text = RESULTADO;
+                DIFERENCIA = VALOR1 - VORIGINAL;
+                lbDiferencia.Text = DIFERENCIA.ToString();
+                TOTAL = decimal.Parse(lbTotal.Text) + DIFERENCIA;
+                tbTotal.Text = string.Format("{0:n}", TOTAL);
+            }
+            catch (Exception error)
+            {
+                SENDER.BackColor = Color.OrangeRed;
+            }
+        }
+
+        private void tbOtros_KeyUp(object sender, KeyEventArgs e)
+        {
+            calcular(tbOtros, tbEfectivo, lbOtros);
+        }
+
+        private void tbEfectivo_KeyUp(object sender, KeyEventArgs e)
+        {
+            calcular(tbEfectivo, tbOtros, lbEfectivo);
         }
     }
 }
