16fa2e261a2375fcca78fec086b7839ecfad509d
CSPFA <cspfaweb@gmail.com>
2018-02-20 11:46:59 -0300

    CORRECCION IMPRESION DE CARNETS FAMILIARES /3

--- Familiares.cs
+++ Familiares.cs
@@ -2055,9 +2055,14 @@ namespace SOCIOS
                     }
                     else
                     {
-                        //aqui
-                        vcadena = "INSERT INTO IDProjectData (IDCf_altci, IDCBarCodeField1, IDCnro_soc, IDCape_soc, IDCNOM_SOC, IDCnro_doc, IDCTIP_DOC, IDCCRJP1, IDCCRJP2, IDCcrjp3, IDCfoto, IDCbarr_1) ";
+                        vcadena = "INSERT INTO IDProjectData (IDCf_altci, IDCBarCodeField1, IDCnro_soc, IDCape_soc, IDCNOM_SOC, IDCnro_doc, IDCTIP_DOC, IDCCRJP1, IDCCRJP2, IDCcrjp3, IDCfoto, IDCsbarra) ";
                         vcadena += " VALUES ('" + IDCf_altci + "', '" + IDCBarCodeField1 + "', '" + IDCnro_soc + "','" + IDCape_soc + "','" + IDCNOM_SOC + "', '" + IDCnro_doc + "', '" + IDCTIP_DOC + "', '" + vp1 + "', '" + vp2 + "', '" + vp3 + "', ?, '" + vaux + "')";
+
+                        if (maskedTextBox2.Text == "13" || maskedTextBox2.Text == "013")
+                        {
+                            vcadena = "INSERT INTO IDProjectData (IDCf_altci, IDCBarCodeField1, IDCnro_soc, IDCape_soc, IDCNOM_SOC, IDCnro_doc, IDCTIP_DOC, IDCCRJP1, IDCCRJP2, IDCcrjp3, IDCfoto, IDCbarr_1) ";
+                            vcadena += " VALUES ('" + IDCf_altci + "', '" + IDCBarCodeField1 + "', '" + IDCnro_soc + "','" + IDCape_soc + "','" + IDCNOM_SOC + "', '" + IDCnro_doc + "', '" + IDCTIP_DOC + "', '" + vp1 + "', '" + vp2 + "', '" + vp3 + "', ?, '" + vaux + "')";
+                        }
 
                         if (maskedTextBox2.Text == "17" || maskedTextBox2.Text == "017")
                         {
