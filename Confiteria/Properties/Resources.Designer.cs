770ba8d20627e7cbc42c1d2b1f0d5fe6fd4b10cc
CSPFA <cspfaweb@gmail.com>
2018-04-23 15:35:00 -0300

    CORRIJO ERRORES CON IMAGENES

--- Confiteria/Properties/Resources.Designer.cs
+++ Confiteria/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Confiteria.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap printer {
+            get {
+                object obj = ResourceManager.GetObject("printer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
