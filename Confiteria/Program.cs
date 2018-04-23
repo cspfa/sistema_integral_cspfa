b488ecfddf87c71444cd18f68e212a5e314443ec
CSPFA <cspfaweb@gmail.com>
2018-04-23 14:37:28 -0300

    CAMBIO EL FORMULARIO POR DEFECTO

--- Confiteria/Program.cs
+++ Confiteria/Program.cs
@@ -15,7 +15,7 @@ namespace Confiteria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new grillaPreComanda());
         }
     }
 }
