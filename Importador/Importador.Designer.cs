ea0df7f0c1f3ab7a084ddd0ed0c14e7e860da9e9
sauladell <sebaauladell@gmail.com>
2018-04-25 18:03:51 -0300

    importacion, deportes

--- Importador/Importador.Designer.cs
+++ Importador/Importador.Designer.cs
@@ -40,27 +40,33 @@
             this.Refrescar_EC = new System.Windows.Forms.Button();
             this.lbEntradas = new System.Windows.Forms.Label();
             this.lbEntradaCampo = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.infoDepAlta = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.InfoDepUPD = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.InfoDepDEL = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnImportarDeportes = new System.Windows.Forms.Button();
+            this.btnFiltrarDeportes = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbResultDeportes = new System.Windows.Forms.Label();
+            this.DepoRes = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.lbResultEC = new System.Windows.Forms.Label();
-            this.DepoRes = new System.Windows.Forms.Label();
-            this.lbResultDeportes = new System.Windows.Forms.Label();
             this.gpErrores = new System.Windows.Forms.GroupBox();
             this.lBerrores = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fechaHasta = new System.Windows.Forms.DateTimePicker();
+            this.fechaDesde = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.gpRed.SuspendLayout();
             this.pnlID.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.gpErrores.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gpRed
@@ -192,16 +198,16 @@
             this.lbEntradaCampo.TabIndex = 32;
             this.lbEntradaCampo.Text = "0";
             // 
-            // label2
+            // infoDepAlta
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(144, 29);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 13);
-            this.label2.TabIndex = 34;
-            this.label2.Text = "0";
+            this.infoDepAlta.AutoSize = true;
+            this.infoDepAlta.BackColor = System.Drawing.Color.White;
+            this.infoDepAlta.ForeColor = System.Drawing.Color.Red;
+            this.infoDepAlta.Location = new System.Drawing.Point(144, 29);
+            this.infoDepAlta.Name = "infoDepAlta";
+            this.infoDepAlta.Size = new System.Drawing.Size(13, 13);
+            this.infoDepAlta.TabIndex = 34;
+            this.infoDepAlta.Text = "0";
             // 
             // label3
             // 
@@ -213,16 +219,16 @@
             this.label3.Text = "DEPORTES ALTA";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // label4
+            // InfoDepUPD
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.White;
-            this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(144, 54);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(13, 13);
-            this.label4.TabIndex = 36;
-            this.label4.Text = "0";
+            this.InfoDepUPD.AutoSize = true;
+            this.InfoDepUPD.BackColor = System.Drawing.Color.White;
+            this.InfoDepUPD.ForeColor = System.Drawing.Color.Red;
+            this.InfoDepUPD.Location = new System.Drawing.Point(144, 54);
+            this.InfoDepUPD.Name = "InfoDepUPD";
+            this.InfoDepUPD.Size = new System.Drawing.Size(13, 13);
+            this.InfoDepUPD.TabIndex = 36;
+            this.InfoDepUPD.Text = "0";
             // 
             // label5
             // 
@@ -233,61 +239,64 @@
             this.label5.TabIndex = 35;
             this.label5.Text = "DEPORTES UPD";
             // 
-            // label6
+            // InfoDepDEL
             // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.White;
-            this.label6.ForeColor = System.Drawing.Color.Red;
-            this.label6.Location = new System.Drawing.Point(144, 79);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(13, 13);
-            this.label6.TabIndex = 38;
-            this.label6.Text = "0";
+            this.InfoDepDEL.AutoSize = true;
+            this.InfoDepDEL.BackColor = System.Drawing.Color.White;
+            this.InfoDepDEL.ForeColor = System.Drawing.Color.Red;
+            this.InfoDepDEL.Location = new System.Drawing.Point(144, 79);
+            this.InfoDepDEL.Name = "InfoDepDEL";
+            this.InfoDepDEL.Size = new System.Drawing.Size(13, 13);
+            this.InfoDepDEL.TabIndex = 38;
+            this.InfoDepDEL.Text = "0";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(20, 79);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.Size = new System.Drawing.Size(90, 13);
             this.label7.TabIndex = 37;
-            this.label7.Text = "DEPORTES UPD";
+            this.label7.Text = "DEPORTES DEL";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.btnImportarDeportes);
+            this.groupBox1.Controls.Add(this.btnFiltrarDeportes);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.InfoDepDEL);
+            this.groupBox1.Controls.Add(this.infoDepAlta);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.InfoDepUPD);
             this.groupBox1.Location = new System.Drawing.Point(15, 130);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(429, 112);
+            this.groupBox1.Size = new System.Drawing.Size(483, 165);
             this.groupBox1.TabIndex = 33;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Deportes";
             this.groupBox1.Visible = false;
             // 
-            // button1
+            // btnImportarDeportes
             // 
-            this.button1.Location = new System.Drawing.Point(314, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 40;
-            this.button1.Text = "IMPORTAR";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnImportarDeportes.Location = new System.Drawing.Point(354, 112);
+            this.btnImportarDeportes.Name = "btnImportarDeportes";
+            this.btnImportarDeportes.Size = new System.Drawing.Size(75, 23);
+            this.btnImportarDeportes.TabIndex = 40;
+            this.btnImportarDeportes.Text = "IMPORTAR";
+            this.btnImportarDeportes.UseVisualStyleBackColor = true;
+            this.btnImportarDeportes.Click += new System.EventHandler(this.btnImportarDeportes_Click);
             // 
-            // button2
+            // btnFiltrarDeportes
             // 
-            this.button2.Location = new System.Drawing.Point(181, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 39;
-            this.button2.Text = "Filtrar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFiltrarDeportes.Location = new System.Drawing.Point(23, 112);
+            this.btnFiltrarDeportes.Name = "btnFiltrarDeportes";
+            this.btnFiltrarDeportes.Size = new System.Drawing.Size(75, 23);
+            this.btnFiltrarDeportes.TabIndex = 39;
+            this.btnFiltrarDeportes.Text = "Filtrar";
+            this.btnFiltrarDeportes.UseVisualStyleBackColor = true;
+            this.btnFiltrarDeportes.Click += new System.EventHandler(this.btnFiltrarDeportes_Click);
             // 
             // groupBox2
             // 
@@ -295,12 +304,32 @@
             this.groupBox2.Controls.Add(this.DepoRes);
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.lbResultEC);
-            this.groupBox2.Location = new System.Drawing.Point(461, 23);
+            this.groupBox2.Location = new System.Drawing.Point(524, 28);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(164, 219);
+            this.groupBox2.Size = new System.Drawing.Size(164, 267);
             this.groupBox2.TabIndex = 34;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "RESULTADOS";
+            // 
+            // lbResultDeportes
+            // 
+            this.lbResultDeportes.AutoSize = true;
+            this.lbResultDeportes.BackColor = System.Drawing.Color.White;
+            this.lbResultDeportes.ForeColor = System.Drawing.Color.Red;
+            this.lbResultDeportes.Location = new System.Drawing.Point(130, 61);
+            this.lbResultDeportes.Name = "lbResultDeportes";
+            this.lbResultDeportes.Size = new System.Drawing.Size(13, 13);
+            this.lbResultDeportes.TabIndex = 44;
+            this.lbResultDeportes.Text = "--";
+            // 
+            // DepoRes
+            // 
+            this.DepoRes.AutoSize = true;
+            this.DepoRes.Location = new System.Drawing.Point(6, 61);
+            this.DepoRes.Name = "DepoRes";
+            this.DepoRes.Size = new System.Drawing.Size(66, 13);
+            this.DepoRes.TabIndex = 43;
+            this.DepoRes.Text = "DEPORTES";
             // 
             // label8
             // 
@@ -322,32 +351,12 @@
             this.lbResultEC.TabIndex = 42;
             this.lbResultEC.Text = "--";
             // 
-            // DepoRes
-            // 
-            this.DepoRes.AutoSize = true;
-            this.DepoRes.Location = new System.Drawing.Point(6, 61);
-            this.DepoRes.Name = "DepoRes";
-            this.DepoRes.Size = new System.Drawing.Size(66, 13);
-            this.DepoRes.TabIndex = 43;
-            this.DepoRes.Text = "DEPORTES";
-            // 
-            // lbResultDeportes
-            // 
-            this.lbResultDeportes.AutoSize = true;
-            this.lbResultDeportes.BackColor = System.Drawing.Color.White;
-            this.lbResultDeportes.ForeColor = System.Drawing.Color.Red;
-            this.lbResultDeportes.Location = new System.Drawing.Point(130, 61);
-            this.lbResultDeportes.Name = "lbResultDeportes";
-            this.lbResultDeportes.Size = new System.Drawing.Size(13, 13);
-            this.lbResultDeportes.TabIndex = 44;
-            this.lbResultDeportes.Text = "--";
-            // 
             // gpErrores
             // 
             this.gpErrores.Controls.Add(this.lBerrores);
-            this.gpErrores.Location = new System.Drawing.Point(15, 258);
+            this.gpErrores.Location = new System.Drawing.Point(21, 301);
             this.gpErrores.Name = "gpErrores";
-            this.gpErrores.Size = new System.Drawing.Size(610, 100);
+            this.gpErrores.Size = new System.Drawing.Size(661, 100);
             this.gpErrores.TabIndex = 35;
             this.gpErrores.TabStop = false;
             this.gpErrores.Text = "ERRORES";
@@ -362,11 +371,54 @@
             this.lBerrores.TabIndex = 0;
             this.lBerrores.Text = "-";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fechaHasta);
+            this.panel1.Controls.Add(this.fechaDesde);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Location = new System.Drawing.Point(181, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 64);
+            this.panel1.TabIndex = 41;
+            // 
+            // fechaHasta
+            // 
+            this.fechaHasta.Location = new System.Drawing.Point(65, 32);
+            this.fechaHasta.Name = "fechaHasta";
+            this.fechaHasta.Size = new System.Drawing.Size(200, 20);
+            this.fechaHasta.TabIndex = 8;
+            // 
+            // fechaDesde
+            // 
+            this.fechaDesde.Location = new System.Drawing.Point(67, 6);
+            this.fechaDesde.Name = "fechaDesde";
+            this.fechaDesde.Size = new System.Drawing.Size(200, 20);
+            this.fechaDesde.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "HASTA";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "DESDE";
+            // 
             // Importador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 545);
+            this.ClientSize = new System.Drawing.Size(700, 545);
             this.Controls.Add(this.gpErrores);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -383,6 +435,8 @@
             this.groupBox2.PerformLayout();
             this.gpErrores.ResumeLayout(false);
             this.gpErrores.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -399,17 +453,17 @@
         private System.Windows.Forms.CheckBox cbID;
         private System.Windows.Forms.Label lbEntradas;
         private System.Windows.Forms.Label lbEntradaCampo;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label infoDepAlta;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button Importar_EC;
         private System.Windows.Forms.Button Refrescar_EC;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label InfoDepUPD;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label InfoDepDEL;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnImportarDeportes;
+        private System.Windows.Forms.Button btnFiltrarDeportes;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label lbResultDeportes;
         private System.Windows.Forms.Label DepoRes;
@@ -417,6 +471,11 @@
         private System.Windows.Forms.Label lbResultEC;
         private System.Windows.Forms.GroupBox gpErrores;
         private System.Windows.Forms.Label lBerrores;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DateTimePicker fechaHasta;
+        private System.Windows.Forms.DateTimePicker fechaDesde;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
 
     }
 }
--- Importador/Importador.cs
+++ Importador/Importador.cs
@@ -14,9 +14,12 @@ namespace Importador
     public partial class Importador : Form
     {
         List<SOCIOS.EntradaCampo> LISTA_EC = new List<SOCIOS.EntradaCampo>();
+        List<SOCIOS.deportes.Deporte_Importacion> LISTA_DEPORTES = new List<SOCIOS.deportes.Deporte_Importacion>();
+
         SOCIOS.bo_Entrada_Campo dlog = new bo_Entrada_Campo();
         EntradaCampoService entradaCampoService = new EntradaCampoService();
-
+        SOCIOS.deportes.DeportesService deporteService = new SOCIOS.deportes.DeportesService();
+       
         //SOCIOS.conString 
 
         private string ROLE { get; set; }
@@ -49,14 +52,17 @@ namespace Importador
 
         private void Refrescar_Entrada_Campo()
         {
-
+            lBerrores.Text = "";
+            lbResultEC.Text = "-";
         LISTA_EC=      entradaCampoService.InfoExportar(chkFiltro.Checked, cbID.Checked, tbDESDE.Text, tbHASTA.Text,ROLE);
         lbEntradaCampo.Text= LISTA_EC.Count.ToString();
         }
 
         private void Importar_EC_Click(object sender, EventArgs e)
         {
-            try {entradaCampoService.Importar_Entrada_Campo(LISTA_EC);
+            try {
+                lBerrores.Text = "";
+                entradaCampoService.Importar_Entrada_Campo(LISTA_EC);
                  lbResultEC.Text = "OK";
                 }
 
@@ -72,6 +78,41 @@ namespace Importador
         {
              gpErrores.Visible=true;
             lBerrores.Text =ERR;
+        }
+
+        private void btnFiltrarDeportes_Click(object sender, EventArgs e)
+        {
+
+            lBerrores.Text = "";
+            lbResultDeportes.Text = "-";
+            this.Filtrar_Deportes();
+        
+        
+        }
+
+        private void Filtrar_Deportes()
+
+        {
+            LISTA_DEPORTES = deporteService.Importar_Exportar_Deporte(FECHAS.fechaUSA(fechaDesde.Value), FECHAS.fechaUSA(fechaHasta.Value), ROLE, true);
+            infoDepAlta.Text = LISTA_DEPORTES.Where(x => x.TIPO.Contains("ALTA")).Count().ToString();
+            InfoDepDEL.Text = LISTA_DEPORTES.Where(x => x.TIPO.Contains("BAJ")).Count().ToString();
+            InfoDepUPD.Text = LISTA_DEPORTES.Where(x => x.TIPO.Contains("MOD")).Count().ToString();
+        }
+
+        private void btnImportarDeportes_Click(object sender, EventArgs e)
+        {
+            try {
+                lBerrores.Text = "";
+                deporteService.Proceso_Importar_Exportar(LISTA_DEPORTES, fechaDesde.Value, fechaHasta.Value,ROLE, true, true);
+                lbResultDeportes.Text = "OK";
+            }
+
+            catch (Exception ex)
+               {
+
+
+                   Error("ERROR DEPORTES:" + ex.Message);
+            }
         }
     
     }
