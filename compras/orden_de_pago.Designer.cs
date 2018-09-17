a775bde5078892525cf37514d500695fb1ef44a7
Andres Hernandez <cspfaweb@gmail.com>
2018-09-17 13:23:07 -0300

    AGREGO FORMULARIO QUE DESGLOSA LAS ORDENES DE PAGO MOSTRANDO CHEQUES, FACTURAS Y TRANSFERENCIAS

---
+++ compras/orden_de_pago.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SOCIOS
+{
+    partial class orden_de_pago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvBuscarOP = new MicroFour.StrataFrame.UI.Windows.Forms.ListView();
+            this.lvFacturas = new MicroFour.StrataFrame.UI.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listView1 = new MicroFour.StrataFrame.UI.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView2 = new MicroFour.StrataFrame.UI.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lvBuscarOP
+            // 
+            this.lvBuscarOP.BackColor = System.Drawing.Color.Gainsboro;
+            this.lvBuscarOP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvBuscarOP.CausesValidation = false;
+            this.lvBuscarOP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvBuscarOP.FullRowSelect = true;
+            this.lvBuscarOP.HideSelection = false;
+            this.lvBuscarOP.Location = new System.Drawing.Point(8, 31);
+            this.lvBuscarOP.Margin = new System.Windows.Forms.Padding(0);
+            this.lvBuscarOP.MultiSelect = false;
+            this.lvBuscarOP.Name = "lvBuscarOP";
+            this.lvBuscarOP.ParentContainer = null;
+            this.lvBuscarOP.ShowItemToolTips = true;
+            this.lvBuscarOP.Size = new System.Drawing.Size(782, 42);
+            this.lvBuscarOP.TabIndex = 5;
+            this.lvBuscarOP.UseCompatibleStateImageBehavior = false;
+            this.lvBuscarOP.View = System.Windows.Forms.View.Details;
+            // 
+            // lvFacturas
+            // 
+            this.lvFacturas.BackColor = System.Drawing.Color.Gainsboro;
+            this.lvFacturas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvFacturas.CausesValidation = false;
+            this.lvFacturas.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvFacturas.FullRowSelect = true;
+            this.lvFacturas.HideSelection = false;
+            this.lvFacturas.Location = new System.Drawing.Point(8, 104);
+            this.lvFacturas.Margin = new System.Windows.Forms.Padding(0);
+            this.lvFacturas.MultiSelect = false;
+            this.lvFacturas.Name = "lvFacturas";
+            this.lvFacturas.ParentContainer = null;
+            this.lvFacturas.ShowItemToolTips = true;
+            this.lvFacturas.Size = new System.Drawing.Size(782, 106);
+            this.lvFacturas.TabIndex = 6;
+            this.lvFacturas.UseCompatibleStateImageBehavior = false;
+            this.lvFacturas.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "FACTURAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DATOS DE LA ORDEN DE PAGO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "CHEQUES";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.Gainsboro;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.CausesValidation = false;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(8, 241);
+            this.listView1.Margin = new System.Windows.Forms.Padding(0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.ParentContainer = null;
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(782, 106);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "TRANSFERENCIAS";
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.Color.Gainsboro;
+            this.listView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView2.CausesValidation = false;
+            this.listView2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView2.FullRowSelect = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(8, 378);
+            this.listView2.Margin = new System.Windows.Forms.Padding(0);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.ParentContainer = null;
+            this.listView2.ShowItemToolTips = true;
+            this.listView2.Size = new System.Drawing.Size(782, 106);
+            this.listView2.TabIndex = 11;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // orden_de_pago
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvFacturas);
+            this.Controls.Add(this.lvBuscarOP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "orden_de_pago";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DETALLE DE LA ORDEN DE PAGO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MicroFour.StrataFrame.UI.Windows.Forms.ListView lvBuscarOP;
+        private MicroFour.StrataFrame.UI.Windows.Forms.ListView lvFacturas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private MicroFour.StrataFrame.UI.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label4;
+        private MicroFour.StrataFrame.UI.Windows.Forms.ListView listView2;
+    }
+}
---
+++ compras/orden_de_pago.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace SOCIOS
+{
+    public partial class orden_de_pago : Form
+    {
+        public orden_de_pago(int OP)
+        {
+            InitializeComponent();
+            buscarOrdenDePago(OP);
+            pintarOpAnulada();
+            buscarFacturas(OP);
+        }
+
+        private void buscarFacturas(int OP)
+        {
+            Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                conString cs = new conString();
+                string connectionString = cs.get();
+
+                using (FbConnection connection = new FbConnection(connectionString))
+                {
+                    connection.Open();
+                    FbTransaction transaction = connection.BeginTransaction();
+                    DataSet ds = new DataSet();
+                    string QUERY = "SELECT F.ID, F.PROVEEDOR, F.NUM_FACTURA, F.FECHA, F.IMPORTE, F.OBSERVACIONES, F.FE_ALTA, F.US_ALTA, F.FE_MOD, ";
+                    QUERY += "F.US_MOD, P.RAZON_SOCIAL, F.SECTOR, F.ORDEN_DE_PAGO, F.NRO_REMITO, F.RETENCION, T.TIPO, P.CUIT, F.TIPO AS TC, F.DESCUENTO, F.ANULADO, F.OP_ANULADA ";
+                    QUERY += "FROM FACTURAS F, PROVEEDORES P, TIPOS_CARGA_COMPROBANTE T ";
+                    QUERY += "WHERE F.ORDEN_DE_PAGO = " + OP + " AND P.ID = F.PROVEEDOR AND F.TIPO = T.ID ORDER BY F.FECHA DESC";
+                    FbCommand cmd = new FbCommand(QUERY, connection, transaction);
+                    cmd.CommandText = QUERY;
+                    cmd.Connection = connection;
+                    cmd.CommandType = CommandType.Text;
+                    FbDataAdapter da = new FbDataAdapter(cmd);
+                    da.Fill(ds);
+
+                    using (FbDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            mostrarResultadosFactura(reader);
+                        }
+                        else
+
+                        {
+                            MessageBox.Show("NO EXISTEN REGISTROS CON LA CONDICION INDICADA", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        }
+
+                        reader.Close();
+                    }
+
+                    transaction.Commit();
+                    connection.Close();
+                    cmd = null;
+                    transaction = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            Cursor = Cursors.Default;
+        }
+
+        private void mostrarResultadosFactura(FbDataReader reader)
+        {
+            lvFacturas.Items.Clear();
+            lvFacturas.Columns.Clear();
+            lvFacturas.BeginUpdate();
+
+            if (lvFacturas.Columns.Count == 0)
+            {
+                lvFacturas.Columns.Add("#");
+                lvFacturas.Columns.Add("PROVEEDOR");
+                lvFacturas.Columns.Add("NUMERO");
+                lvFacturas.Columns.Add("FECHA");
+                lvFacturas.Columns.Add("IMPORTE");
+                lvFacturas.Columns.Add("OBSERVACIONES");
+                lvFacturas.Columns.Add("SECTOR");
+                lvFacturas.Columns.Add("TIPO");
+                lvFacturas.Columns.Add("DES%");
+                lvFacturas.Columns.Add("ANULADA");
+            }
+            do
+            {
+                decimal IMPORTE = reader.GetDecimal(reader.GetOrdinal("IMPORTE"));
+                string VALOR = string.Format("{0:n}", IMPORTE);
+                DateTime FECHA = reader.GetDateTime(reader.GetOrdinal("FECHA"));
+                ListViewItem listItem = new ListViewItem(reader.GetString(reader.GetOrdinal("ID")).Trim());
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("RAZON_SOCIAL")).Trim());
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("NUM_FACTURA")).Trim());
+                listItem.SubItems.Add(FECHA.ToShortDateString());
+                listItem.SubItems.Add(VALOR);
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("OBSERVACIONES")).Trim());
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("SECTOR")).Trim());
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("TIPO")).Trim());
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("DESCUENTO")).Trim());
+                listItem.SubItems.Add(reader.GetString(reader.GetOrdinal("ANULADO")).Trim());
+                lvFacturas.Items.Add(listItem);
+            }
+
+            while (reader.Read());
+            lvFacturas.EndUpdate();
+            lvFacturas.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            lvFacturas.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void buscarOrdenDePago(int NRO_OP)
+        {
+            Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                conString cs = new conString();
+                string connectionString = cs.get();
+
+                using (FbConnection connection = new FbConnection(connectionString))
+                {
+                    connection.Open();
+                    FbTransaction transaction = connection.BeginTransaction();
+                    DataSet ds = new DataSet();
+                    string QUERY = "SELECT ID, FECHA_OP, BENEFICIARIO, TOTAL, OBSERVACIONES, ANULA_FECHA, CANULA_FECHA, CANCELA_FECHA FROM ORDENES_DE_PAGO WHERE ID = " + NRO_OP;
+                    FbCommand cmd = new FbCommand(QUERY, connection, transaction);
+                    cmd.CommandText = QUERY;
+                    cmd.Connection = connection;
+                    cmd.CommandType = CommandType.Text;
+                    FbDataAdapter da = new FbDataAdapter(cmd);
+                    da.Fill(ds);
+
+                    using (FbDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            mostrarOrdenesDePago(reader);
+                        }
+                        else
+                        {
+                            MessageBox.Show("NO EXISTEN REGISTROS CON LA CONDICION INDICADA", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        }
+                    }
+
+                    transaction.Commit();
+                    connection.Close();
+                    cmd = null;
+                    transaction = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            Cursor = Cursors.Default;
+        }
+
+        private void mostrarOrdenesDePago(FbDataReader OPS)
+        {
+            lvBuscarOP.Items.Clear();
+            lvBuscarOP.Columns.Clear();
+            lvBuscarOP.BeginUpdate();
+
+            if (lvBuscarOP.Columns.Count == 0)
+            {
+                lvBuscarOP.Columns.Add("# OP");
+                lvBuscarOP.Columns.Add("FECHA");
+                lvBuscarOP.Columns.Add("BENEFICIARIO");
+                lvBuscarOP.Columns.Add("IMPORTE");
+                lvBuscarOP.Columns.Add("OBSERVACIONES");
+                lvBuscarOP.Columns.Add("ANULADA");
+                lvBuscarOP.Columns.Add("CONFIRMADA");
+                lvBuscarOP.Columns.Add("CANCELADA");
+            }
+            do
+            {
+                string ID = OPS.GetString(OPS.GetOrdinal("ID")).Trim();
+                string FECHA = OPS.GetString(OPS.GetOrdinal("FECHA_OP")).Trim().Substring(0, 10);
+                string IMPORTE = string.Format("{0:n}", OPS.GetDecimal(OPS.GetOrdinal("TOTAL")));
+                string BENEFICIARIO = OPS.GetString(OPS.GetOrdinal("BENEFICIARIO")).Trim();
+                string OBSERVACIONES = OPS.GetString(OPS.GetOrdinal("OBSERVACIONES")).Trim();
+                string ANULA_FECHA = "";
+                string CANULA_FECHA = "";
+                string CANCELA_FECHA = "";
+
+                if (OPS.GetString(OPS.GetOrdinal("ANULA_FECHA")) != "")
+                {
+                    ANULA_FECHA = OPS.GetString(OPS.GetOrdinal("ANULA_FECHA")).Trim().Substring(0, 10);
+                }
+
+                if (OPS.GetString(OPS.GetOrdinal("CANULA_FECHA")) != "")
+                {
+                    CANULA_FECHA = OPS.GetString(OPS.GetOrdinal("CANULA_FECHA")).Trim().Substring(0, 10);
+                }
+
+                if (OPS.GetString(OPS.GetOrdinal("CANCELA_FECHA")) != "")
+                {
+                    CANCELA_FECHA = OPS.GetString(OPS.GetOrdinal("CANCELA_FECHA")).Trim().Substring(0, 10);
+                }
+
+                ListViewItem listItem = new ListViewItem(ID);
+                listItem.SubItems.Add(FECHA);
+                listItem.SubItems.Add(BENEFICIARIO);
+                listItem.SubItems.Add(IMPORTE);
+                listItem.SubItems.Add(OBSERVACIONES);
+                listItem.SubItems.Add(ANULA_FECHA);
+                listItem.SubItems.Add(CANULA_FECHA);
+                listItem.SubItems.Add(CANCELA_FECHA);
+                lvBuscarOP.Items.Add(listItem);
+            }
+
+            while (OPS.Read());
+            lvBuscarOP.EndUpdate();
+            lvBuscarOP.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            lvBuscarOP.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void pintarOpAnulada()
+        {
+            if (lvBuscarOP.Items.Count > 0)
+            {
+                foreach (ListViewItem row in lvBuscarOP.Items)
+                {
+                    if (row.SubItems[5].Text != "")
+                    {
+                        row.BackColor = Color.Orange;
+                        row.ForeColor = Color.Black;
+                    }
+                    if (row.SubItems[6].Text != "")
+                    {
+                        row.BackColor = Color.Red;
+                        row.ForeColor = Color.White;
+                    }
+                }
+            }
+        }
+    }
+}
