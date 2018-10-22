a71509a56206cf7540d7651bb3eb7e6b59ba67dd
sauladell <sebaauladell@gmail.com>
2018-10-22 14:14:05 -0300

    facturacion electronica consumidor

--- Factura_Electronica/Enums.cs
+++ Factura_Electronica/Enums.cs
@@ -7,8 +7,9 @@ namespace SOCIOS.Factura_Electronica
 {
    public  enum Tipo_Doc_Enum
     {
-       DNI =96,
-       CUIT = 80
+       DNI              =96,
+       CUIT             = 80,
+       CONSUMIDOR_FINAL =99
 
     }
 
