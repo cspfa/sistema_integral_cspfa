e4a97e01fb300ed71cfbb38968c66dca066aae8c
sauladell <sebaauladell@gmail.com>
2020-04-01 16:56:22 -0300

    interior: se agrega pantalla de detalle de dias consumidos

---
+++ interior/Disponibilidad_Dias.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace SOCIOS.interior
+{
+    partial class Disponibilidad_Dias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbDias = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbAnio = new System.Windows.Forms.Label();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTramite = new System.Windows.Forms.Label();
+            this.lbCirugia = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbEnfermedad = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbDias
+            // 
+            this.lbDias.AutoSize = true;
+            this.lbDias.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDias.ForeColor = System.Drawing.Color.Blue;
+            this.lbDias.Location = new System.Drawing.Point(55, 31);
+            this.lbDias.Name = "lbDias";
+            this.lbDias.Size = new System.Drawing.Size(49, 18);
+            this.lbDias.TabIndex = 0;
+            this.lbDias.Text = "lbDias";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vouchers";
+            // 
+            // lbAnio
+            // 
+            this.lbAnio.AutoSize = true;
+            this.lbAnio.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAnio.ForeColor = System.Drawing.Color.Blue;
+            this.lbAnio.Location = new System.Drawing.Point(226, 9);
+            this.lbAnio.Name = "lbAnio";
+            this.lbAnio.Size = new System.Drawing.Size(13, 18);
+            this.lbAnio.TabIndex = 2;
+            this.lbAnio.Text = "-";
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(12, 105);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(392, 150);
+            this.dgvData.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(420, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CONSUMOS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(420, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TRAMITE";
+            // 
+            // lbTramite
+            // 
+            this.lbTramite.AutoSize = true;
+            this.lbTramite.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTramite.ForeColor = System.Drawing.Color.Blue;
+            this.lbTramite.Location = new System.Drawing.Point(469, 161);
+            this.lbTramite.Name = "lbTramite";
+            this.lbTramite.Size = new System.Drawing.Size(13, 18);
+            this.lbTramite.TabIndex = 6;
+            this.lbTramite.Text = "-";
+            // 
+            // lbCirugia
+            // 
+            this.lbCirugia.AutoSize = true;
+            this.lbCirugia.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCirugia.ForeColor = System.Drawing.Color.Brown;
+            this.lbCirugia.Location = new System.Drawing.Point(469, 206);
+            this.lbCirugia.Name = "lbCirugia";
+            this.lbCirugia.Size = new System.Drawing.Size(13, 18);
+            this.lbCirugia.TabIndex = 8;
+            this.lbCirugia.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label6.Location = new System.Drawing.Point(420, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "CIRUGIA";
+            // 
+            // lbEnfermedad
+            // 
+            this.lbEnfermedad.AutoSize = true;
+            this.lbEnfermedad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEnfermedad.ForeColor = System.Drawing.Color.Violet;
+            this.lbEnfermedad.Location = new System.Drawing.Point(469, 251);
+            this.lbEnfermedad.Name = "lbEnfermedad";
+            this.lbEnfermedad.Size = new System.Drawing.Size(13, 18);
+            this.lbEnfermedad.TabIndex = 10;
+            this.lbEnfermedad.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label8.Location = new System.Drawing.Point(420, 224);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 18);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "ENFERMEDAD";
+            // 
+            // Disponibilidad_Dias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 319);
+            this.Controls.Add(this.lbEnfermedad);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbCirugia);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbTramite);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.lbAnio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbDias);
+            this.Name = "Disponibilidad_Dias";
+            this.Text = "Disponibilidad_Dias";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbDias;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbAnio;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTramite;
+        private System.Windows.Forms.Label lbCirugia;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbEnfermedad;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ interior/Disponibilidad_Dias.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SOCIOS.Turismo; 
+namespace SOCIOS.interior
+{
+    public partial class Disponibilidad_Dias : Form
+    {
+        Hotel_Dias_Utils hotel_Dias_Utils = new Hotel_Dias_Utils();
+        Datos_Dias service_Datos_Dias;
+        public Disponibilidad_Dias(int Socio,int Dep,int Anio)
+        {
+            InitializeComponent();
+
+            lbDias.Text = hotel_Dias_Utils.ConsultarDias(Socio, Dep, Anio);
+            service_Datos_Dias = new Datos_Dias(Socio, Dep);
+            dgvData.DataSource = service_Datos_Dias.DETALLE;
+
+            lbCirugia.Text = service_Datos_Dias.CIRUGIA.ToString();
+            lbEnfermedad.Text = service_Datos_Dias.ENFERMEDAD.ToString();
+            lbTramite.Text = service_Datos_Dias.TRAMITE.ToString();
+
+
+
+
+        }
+    }
+}
